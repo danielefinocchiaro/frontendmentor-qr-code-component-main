ae90b9ba2e47b604b958d3ea1c76460d8e780821
“danielefinocchiaro” <danielefinocchiaro@keix.com>
2023-02-26 23:31:39 +0100

    Challenge Ended

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import QR_IMAGE from "./assets/image-qr-code.png";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen flex justify-center items-center">
+      <div className="flex flex-col items-center md:w-1/2 lg:w-[23%] md:h-[75%] lg:h-[65%] mx-8 md:mx-0 bg-white rounded-3xl p-5 pb-1">
+        <div className="">
+          <img src={QR_IMAGE} alt="QR code image" className="rounded-xl" />
+        </div>
+        <div className="flex flex-col justify-around grow mx-1  md:mx-8 py-6 text-center">
+          <h1 className="text-xl  md:text-2xl font-bold text-dark-blue">
+            Improve your front-end skills by building projects
+          </h1>
+          <p className="text-base text-grayish-blue mx-1 mt-4  md:mt-0">
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
-
-export default App
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "376px",
+        md: "568px",
+        lg: "924px",
+        xl: "1280px",
+      },
+      colors: {
+        "light-gray": "#d6e2f0",
+        "grayish-blue": "	#7b879d",
+        "dark-blue": "#1f3251",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
